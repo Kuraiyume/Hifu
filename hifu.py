887c1f91874854ff6b94cad18d53e4108f3a5f90
DarkSeraph <114529635+Kuraiyume@users.noreply.github.com>
2024-11-16 22:49:15 +0800

    Add files via upload

--- hifu.py
+++ hifu.py
@@ -9,6 +9,9 @@ import secrets
 import re
 from termcolor import colored 
 import rstr
+import math
+import requests
+import hashlib
 
 banner = r"""
 .___.__  .___ ._______.____
@@ -104,6 +107,27 @@ def evaluate_password_strength(password):
         score += 20
     return min(score, 100)
 
+def calculate_entropy(password):
+    """
+    Calculate the entropy of a given password.
+    Entropy = log2(pool_size^password_length)
+    :param password: Password to evaluate
+    :return: Entropy in bits
+    """
+    pool_size = 0
+    if any(c.isdigit() for c in password):
+        pool_size += 10
+    if any(c.islower() for c in password):
+        pool_size += 26
+    if any(c.isupper() for c in password):
+        pool_size += 26
+    if any(c in string.punctuation for c in password):
+        pool_size += len(string.punctuation)
+    if pool_size == 0:
+        return 0
+    entropy = math.log2(pool_size) * len(password)
+    return round(entropy, 2)
+
 def colorize_strength(strength):
     """Colorize the strength value based on the percentage."""
     if strength >= 80:
@@ -119,6 +143,66 @@ def print_colored_status(status_type, message):
         print(colored("[+]", 'green') + " " + message)
     elif status_type == '[-]':
         print(colored("[-]", 'red') + " " + message)
+
+def check_password_pwned(password):
+    """
+    Check if the provided password has been compromised in any known data breaches.
+    :param password: The password to check.
+    :return: A dictionary containing the breach status, SHA-1 hash, breach count, 
+            and a detailed message about the password's safety.
+    """
+    try:
+        sha1_password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+        prefix = sha1_password[:5]
+        suffix = sha1_password[5:]
+        response = requests.get(f'https://api.pwnedpasswords.com/range/{prefix}')
+        response.raise_for_status()
+        if response.status_code == 200:
+            hashes = (line.split(':') for line in response.text.splitlines())
+            for h, count in hashes:
+                if h == suffix:
+                    status_message = f"{colored('[!]', 'red')} Identified in data breaches. It is strongly recommended to change this password immediately."
+                    sha1_message = f"{colored('[!]', 'red')} SHA1: {sha1_password.lower()}"
+                    breach_count_message = f"{colored('[!]', 'red')} Breach Count: {count}"
+                    return {
+                        'status': f"{colored('[!]', 'red')} Status: Found",
+                        'sha1_message': sha1_message,
+                        'breach_count_message': breach_count_message,
+                        'message': status_message
+                    }
+            status_message = f"{colored('[+]', 'green')} Not detected in any data breaches."
+            sha1_message = f"{colored('[+]', 'green')} SHA1: {sha1_password.lower()}"
+            breach_count_message = f"{colored('[+]', 'green')} Breach Count: 0"
+            return {
+                'status': f"{colored('[+]', 'green')} Status: Not Found",
+                'sha1_message': sha1_message,
+                'breach_count_message': breach_count_message,
+                'message': status_message
+            }
+    except requests.ConnectionError:
+        status_message = f"{colored('[-]', 'red')} Unable to connect to the internet. Please check your internet connection."
+        return {
+            'status': f"{colored('[-]', 'red')} An error occurred",
+            'message': status_message
+        }
+    except requests.HTTPError as http_err:
+        status_message = f"{colored('[-]', 'red')} {http_err}"
+        return {
+            'status': f"{colored('[-]', 'red')} An error occurred",
+            'message': status_message
+        }
+    except requests.Timeout:
+        status_message = f"{colored('[-]', 'red')} The request took too long to complete. Try again later."
+        return {
+            'status': f"{colored('[-]', 'red')} An error occurred",
+            'message': status_message
+        }
+    except Exception as err:
+        status_message = f"{colored('[-]', 'red')} {err}"
+        return {
+            'status': f"{colored('[-]', 'red')} An error occurred",
+            'message': status_message
+        }
 
 def parse_arguments():
     """Parse command-line arguments."""
@@ -139,13 +223,15 @@ def parse_arguments():
     parser.add_argument("-a", "--amount", default=1, type=validate_positive_int, help="Number of passwords to generate")
     parser.add_argument("-r", "--regex", default='', type=str, help="Regex pattern for generating passwords")
     parser.add_argument("-o", "--output-file", help="File to write the generated passwords to")
+    parser.add_argument("-pfix", "--prefix", default='', type=str, help="Prefix to add to each password")
+    parser.add_argument("-sfix", "--suffix", default='', type=str, help="Suffix to add to each password")
+    parser.add_argument("-t", "--total-length", default=0, type=validate_positive_int, help="Total length of the password (including prefix and suffix) If 0, use specified counts")
+    parser.add_argument("-c", "--custom", default='', type=str, help="Custom name for the password with leet speak conversion")
+    parser.add_argument("-p", "--password", default=0, type=str, help="Specify password for checking if the password is breached")
+    parser.add_argument("--check-if-pwned", action='store_true', help="Check if the password from '--password' is breached")
+    parser.add_argument("--seed", type=int, help="Seed for randomization to allow reproducible results")
     parser.add_argument("--output-format", choices=['txt', 'json'], default='txt', help="Format of the output file (txt or json)")
     parser.add_argument("--exclude-chars", default='', type=str, help="Characters to exclude from the password")
-    parser.add_argument("--prefix", default='', type=str, help="Prefix to add to each password")
-    parser.add_argument("--suffix", default='', type=str, help="Suffix to add to each password")
-    parser.add_argument("--total-length", default=0, type=validate_positive_int, help="Total length of the password (including prefix and suffix) If 0, use specified counts")
-    parser.add_argument("--seed", type=int, help="Seed for randomization to allow reproducible results")
-    parser.add_argument("--custom", default='', type=str, help="Custom name for the password with leet speak conversion")
     args = parser.parse_args()
     if len(sys.argv) == 1:
         parser.print_help()
@@ -154,8 +240,24 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
+    if args.check_if_pwned:
+        if not args.password:
+            print_colored_status('[-]', "You must provide a password to check using --password.")
+            sys.exit(1)
+        result = check_password_pwned(args.password)
+        print(result['status'])
+        if 'sha1_message' in result:
+            print(result['sha1_message'])
+        if 'breach_count_message' in result:
+            print(result['breach_count_message'])
+        print(result['message'])
+        sys.exit(1)
+    if args.check_if_pwned and args.password and (args.custom or args.numbers > 0 or 
+            args.lowercase > 0 or args.uppercase > 0 or args.special_chars > 0 or args.total_length > 0):
+        print_colored_status('[-]', "Check Pwned cannot be combined with other generation options.")
+        sys.exit(1)
     if args.regex and (args.custom or args.numbers > 0 or args.lowercase > 0 or
-            args.uppercase > 0 or args.special_chars > 0 or args.total_length > 0):
+            args.uppercase > 0 or args.special_chars > 0 or args.total_length > 0 or args.check_if_pwned > 0 or args.password > 0):
         print_colored_status('[-]', "Regex cannot be combined with other generation options.")
         sys.exit(1)
     if args.seed is not None:
@@ -198,7 +300,9 @@ def main():
             )
             strength = evaluate_password_strength(password)
             colorized_strength = colorize_strength(strength)
-            passwords.append(f"{password} ({colorized_strength})")
+            entropy = calculate_entropy(password)
+            entropy_display = colored(f"{entropy}", 'green')
+            passwords.append(f"{password} ({colorized_strength}, {entropy_display})")
         except ValueError as e:
             print_colored_status('[-]', f"Error generating password: {e}")
             sys.exit(1)
